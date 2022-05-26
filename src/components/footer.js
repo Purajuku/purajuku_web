6bedb4a2a990cd4a33712ffd4a69b9ce9143eb70
tererun <27857171+tererun@users.noreply.github.com>
2022-05-27 00:44:24 +0900

    フッターきれいに

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,11 +3,13 @@ import * as React from "react";
 import "../footer.css"
 
 const GlobalFooter = () => (
-    <footer>
-        © {new Date().getFullYear()}
-        {` `}
-        <a href="https://purajuku.dev/">プラ塾</a>
-    </footer>
+    <div className="global-footer">
+        <footer>
+            © {new Date().getFullYear()}
+            {` `}
+            <a href="https://purajuku.dev/">プラ塾</a>
+        </footer>
+    </div>
 );
 
 export default GlobalFooter;
