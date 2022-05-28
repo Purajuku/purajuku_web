10cafe97e54e74d49e864ed18f269ca41e150a14
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2022-05-28 11:54:29 +0000

    chore: fix quote

--- src/components/home_contents.js
+++ src/components/home_contents.js
@@ -3,7 +3,7 @@ import * as React from "react";
 import Card from "./card";
 import BigCard from "./big_card";
 import MainButton from "./button";
-import Seo from './seo';
+import Seo from "./seo";
 
 import "../home_contents.css"
 
